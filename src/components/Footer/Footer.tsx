111d4b9cb10bbc683d4f07c330ab96c15463bf2b
Weranika <veronikalopanova@gmail.com>
2022-11-23 17:23:20 +0100

    refactor: add variables to css

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,41 +4,43 @@ import './Footer.scss';
 function Footer() {
   return (
     <footer className="footer">
-      <a
-        className="footer__rs-logo-link"
-        href="https://rs.school/index.html"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div className="footer__rs-logo" />
-      </a>
-      <div className="footer__github-info">
+      <div className="footer__container">
         <a
-          className="footer__github-link"
-          href="https://github.com/Weranika"
+          className="footer__rs-logo-link"
+          href="https://rs.school/index.html"
           target="_blank"
           rel="noreferrer"
         >
-          Weranika
+          <div className="footer__rs-logo" />
         </a>
-        <a
-          className="footer__github-link"
-          href="https://github.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          oxanadanilova
-        </a>
-        <a
-          className="footer__github-link"
-          href="https://github.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          shishel-zaitcevich
-        </a>
+        <div className="footer__github-info">
+          <a
+            className="footer__github-link"
+            href="https://github.com/Weranika"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Weranika
+          </a>
+          <a
+            className="footer__github-link"
+            href="https://github.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            oxanadanilova
+          </a>
+          <a
+            className="footer__github-link"
+            href="https://github.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            shishel-zaitcevich
+          </a>
+        </div>
+        <p className="footer__year">2022</p>
       </div>
-      <p className="footer__year">2022</p>
     </footer>
   );
 }
